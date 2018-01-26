ee24f01d5219477c3def37ea98784f5eb850ffdf
Florian Mounier <florian.mounier@kozea.fr>
2018-01-26 14:27:04 +0100

    Add decorator support.

--- config.js
+++ config.js
@@ -75,6 +75,7 @@ module.exports = function getBaseConfig(
           plugins: [
             '@babel/plugin-syntax-dynamic-import',
             '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-proposal-decorators',
           ],
         },
       },
